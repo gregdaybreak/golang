3b1a7909f7e9c9476abcdb74d013e356345d13fa
Greg Brown <greg.brown@lootcrate.com>
2017-09-19 17:03:38 -0700

    make the int's int64's to handle higher primes

--- fermat/fermat.go
+++ fermat/fermat.go
@@ -10,9 +10,9 @@ import (
 )
 
 func main() {
-	arg, _ := strconv.Atoi(os.Args[1])       //convert argument to int
-	myMap := map[bool]int{true: 0, false: 0} //new map to hold bool count
-	for i := 0; i < 100; i++ {               //run 100 tests to be sure if it is prime or not
+	arg, _ := strconv.ParseInt(os.Args[1], 10, 64) //convert argument to int
+	myMap := map[bool]int{true: 0, false: 0}       //new map to hold bool count
+	for i := 0; i < 100; i++ {                     //run 100 tests to be sure if it is prime or not
 		if Fermat(arg) == false { //run the Fermat function and get result
 			myMap[false]++ //increment false
 		} else {
@@ -27,11 +27,11 @@ func main() {
 }
 
 //Fermat tests primality using Fermat's Little Theorem
-func Fermat(num int) bool {
+func Fermat(num int64) bool {
 	rand.Seed(time.Now().UTC().UnixNano())            //seed random number to time
-	randomNum := rand.Intn(num-1) + 1                 //pick a random number between 1 and the number
+	randomNum := rand.Int63n(num-1) + 1               //pick a random number between 1 and the number
 	x := math.Pow(float64(randomNum), float64(num-1)) //calculate randomNumber^(num-1)
-	if int(x)%num != 1 {                              //if randomNumber^(num-1) is not equal to 1
+	if int64(x)%num != 1 {                            //if randomNumber^(num-1) is not equal to 1
 		return false //it is not a prime number
 	}
 	return true //if it is equal to 1 it is prime
